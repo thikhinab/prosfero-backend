9eddc8c965477b8cf29e8a72181808eeed76b510
Thikhina Bakmeedeniya <thikhinab@gmail.com>
2021-07-18 00:58:19 +0800

    set up chat server

--- routes/conversation.js
+++ routes/conversation.js
@@ -3,14 +3,27 @@ const Conversation = require("../models/conversation");
 
 // Create new conversation
 router.post("/", async (req, res) => {
-  const newConversation = new Conversation({
-    members: [req.body.senderId, req.body.receiverId],
-  });
-
   try {
-    const savedConversation = await newConversation.save();
-    res.status(200).json(savedConversation);
+    const conversation = await Conversation.find({
+      members: { $all: [req.body.senderId, req.body.receiverId] },
+    });
+
+    if (conversation.length === 0) {
+      const newConversation = new Conversation({
+        members: [req.body.senderId, req.body.receiverId],
+      });
+
+      try {
+        const savedConversation = await newConversation.save();
+        res.status(200).json(savedConversation);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(200).json("already in conversation");
+    }
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
